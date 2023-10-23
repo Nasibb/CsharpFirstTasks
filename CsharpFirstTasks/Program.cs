ed2172030b3cf87bfc3cf636955cea49f62f0b96
Nasibb <nasibgi@code.edu.az>
2023-10-23 15:41:50 +0400

    1 and 2 Done

---
+++ CsharpFirstTasks/Program.cs
@@ -0,0 +1,18 @@
+﻿#region Task1
+//1) 4 reqemli eded verilib. Bu ededin eveline 7 reqemini , axirinada 8 reqemini artir. Example1: 333 = 73338.
+
+//int num = 1234;
+//int newNum =int.Parse("7"+num.ToString()+"8");
+//Console.WriteLine(newNum);
+
+#endregion
+
+
+#region Task2
+//2) 3 reqemli eded verilib. Bu ededin axirina hemin ededdin ozunu yapishdir. example: 333 = 333333;
+
+//int num = 1234;
+//int newNum = int.Parse(num.ToString() + num);
+//Console.WriteLine(newNum);
+
+#endregion
