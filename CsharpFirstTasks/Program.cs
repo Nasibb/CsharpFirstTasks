5fb269319132f42d8d8f110fc9f7fa48f92c5e94
Nasibb <nasibgi@code.edu.az>
2023-10-24 11:32:20 +0400

    7 Tasks done

--- CsharpFirstTasks/Program.cs
+++ CsharpFirstTasks/Program.cs
@@ -7,12 +7,72 @@
 
 #endregion
 
-
 #region Task2
 //2) 3 reqemli eded verilib. Bu ededin axirina hemin ededdin ozunu yapishdir. example: 333 = 333333;
 
 //int num = 1234;
 //int newNum = int.Parse(num.ToString() + num);
 //Console.WriteLine(newNum);
+
+#endregion
+
+#region Task3
+//3) 5 reqemli eded verilib. Bu ededin evvel 18 % sonra ise 3 % tap.
+
+//int num = 12345;
+//double percent1 = 0.18;
+//double percent2 = 0.03;
+//double result = num * percent1 * percent2;
+//Console.WriteLine(result);
+
+#endregion
+
+#region Task 4
+//4) 3 reqemli eded verilib. Bu ededin axirina 7 artir. Sonra cavabin 7% tap;
+
+//int num = 123;
+//int newNum = int.Parse(num.ToString() + "7");
+//double percent = 0.07;
+//double result = newNum * percent;
+//Console.WriteLine(result);
+
+
+
+#endregion
+
+#region Task 5
+//5) 4 reqemli eded verilib. Bu ededin evvelin 4 reqemi , axirina ise 44 ededini artir. Alinan cavabin 44 % tap.
+
+//int num = 1234;
+//int newNum=int.Parse("4"+num.ToString()+"44");
+//double percent = 0.44;
+//double newNum2 = newNum * percent;
+//Console.WriteLine(newNum2);
+
+
+#endregion
+
+#region Task 6
+//6) 4 reqemli eded verilib. Bu ededin evvel 20%-ni , sonra ise cavabin 10% tap. Alinan cavabin kvadratini tap.
+
+//int num = 1234;
+//double percent1 = 0.2;
+//double percent2 = 0.1;
+//double newNum =num*percent1*percent2;
+//double newNum2 = newNum * newNum;
+//Console.WriteLine(newNum2);
+
+#endregion
+
+#region Task 7
+//7) 2 dene 5 reqemli eded verilib. Bu ededleri toplayib. Alinan cavabin evveline ve axirina 5 artir. Neticenin 5 % tap.
+
+int num1 = 12345;
+int num2 = 12345;
+int sum = num1 + num2;
+int newNum = int.Parse("5"+sum.ToString()+"5");
+double percent = 0.05;
+double newNum2 = newNum * percent;
+Console.WriteLine(newNum2);
 
 #endregion
